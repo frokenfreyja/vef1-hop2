9c8f8099000e2af8a0040db45a3d760707ae2b23
sbs61@hi.is <sbs61@hi.is>
2018-11-22 16:26:08 +0000

    grid og header byrjun

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import List from './lib/list';
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
-
   if (isLecturePage) {
-
-  } else {
+} else {
     const list = new List();
     list.load();
   }
