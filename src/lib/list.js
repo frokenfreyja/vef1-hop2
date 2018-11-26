523a0f0a5066a3173453e9239ace1a5fa66b8750
sbs61 <sbs61@hi.is>
2018-11-26 01:45:39 +0000

    forsida

--- src/lib/list.js
+++ src/lib/list.js
@@ -6,6 +6,58 @@ export default class List {
   }
 
   load() {
-    empty(this.container);
+    fetch('../lectures.json')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Something went wrong on api server!');
+      })
+      .then((data) => {
+        this.parseData(data.lectures);
+      })
+      .catch((error) => {
+        console.error(error); // eslint-disable-line
+      });
+  }
+
+  parseData(data) {
+    this.videos = data;
+    this.getVideos();
+  }
+
+  getVideos() {
+    this.videos.forEach((data) => {
+      const column = document.createElement('section');
+      column.classList.add('col');
+
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      const videoThumbnail = document.createElement('img');
+      videoThumbnail.classList.add('video__thumbnail');
+      videoThumbnail.src = data.thumbnail;
+
+      const videoFooter = document.createElement('div');
+      videoFooter.classList.add('video__footer');
+
+      const videoCategory = document.createElement('div');
+      videoCategory.classList.add('video__category');
+      videoCategory.appendChild(document.createTextNode(data.category));
+
+      const videoTitle = document.createElement('h2');
+      videoTitle.appendChild(document.createTextNode(data.title));
+
+      videoFooter.appendChild(videoCategory);
+      videoFooter.appendChild(videoTitle);
+
+      card.appendChild(videoThumbnail);
+      card.appendChild(videoFooter);
+
+      column.appendChild(card);
+
+
+      this.container.appendChild(column);
+    });
   }
 }
